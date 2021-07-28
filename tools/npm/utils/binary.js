48f27cc6919b28ced67b844850ce9de404efdb94
Ales Rechtorik <foxxcz@gmail.com>
2021-07-28 14:10:09 +0200

    windows download path fix

--- tools/npm/utils/binary.js
+++ tools/npm/utils/binary.js
@@ -7,9 +7,10 @@ function getPlatform() {
   if (type === "Linux" && arch === "x64") return "linux-amd64";
   if (type === "Linux" && arch === "x86") return "linux-i386";
   if (type === "Darwin" && arch === "x64") return "darwin-amd64";
-  if (type === "Windows_NT" && arch === "x64") return "win-x64";
+  if (type === "Windows_NT" && arch === "x64") return "win-x64.exe";
   throw new Error(`Unsupported platform: ${type} ${arch}`);
 }
+
 function getBinary() {
   const platform_arch = getPlatform();
   const version = require("../package.json").version;
@@ -17,25 +18,24 @@ function getBinary() {
   const url = `https://github.com/zeropsio/zcli/releases/download/${compatibleVersion}/zcli-${platform_arch}-npm.tar.gz`;
   const name = `zcli-${platform_arch}`;
 
-  console.log('Downloading '+ name +' from', url);
-
   return new Binary(name, url);
 }
+
 const run = () => {
   const binary = getBinary();
-  console.log('Running binary');
   binary.run();
 };
+
 const install = () => {
   const binary = getBinary();
-  console.log('Installing binary');
   binary.install();
 };
+
 const uninstall = () => {
   const binary = getBinary();
-  console.log('Uninstalling binary');
   binary.uninstall();
 };
+
 module.exports = {
   install,
   run,
