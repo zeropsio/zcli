5f23e3f74f0c0b1dd22999706d49878dc4efcf6d
Jan Saidl <saidl@vshosting.cz>
2021-07-28 18:30:55 +0200

    add user config dir fix

--- src/constants/linux.go
+++ src/constants/linux.go
@@ -2,18 +2,9 @@
 
 package constants
 
-import (
-	"os"
-	"path"
-)
-
 const (
 	LogFilePath           = "/var/log/zerops/zerops.log"
 	SocketFilePath        = "/run/zerops/daemon.sock"
 	DaemonStorageFilePath = "/var/lib/zerops/daemon.data"
 	DaemonInstallDir      = "/usr/local/"
 )
-
-func CliStorageFile() string {
-	return path.Join(os.Getenv("HOME"), "/.config/zerops/cli.data")
-}
--- src/constants/params.go
+++ src/constants/params.go
@@ -1,8 +1,21 @@
 package constants
 
+import (
+	"os"
+	"path"
+)
+
 const (
 	PersistentParamCaCertificateUrl = "caCertificateUrl"
 	PersistentParamRestApiAddress   = "restApiAddress"
 	PersistentParamGrpcApiAddress   = "grpcApiAddress"
 	PersistentParamVpnApiAddress    = "vpnApiAddress"
 )
+
+func CliStorageFile() (string, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+	return path.Join(configDir, ".config", "zerops", "cli.data"), nil
+}
