b61a1732c14962d34f8664dafa4548e71b3d2ef4
Yoshi <kawaii.yoshiyuki@gmail.com>
2022-05-11 12:08:06 +0200

    split service subcommands from command

---
+++ src/cmd/serviceStart.go
@@ -0,0 +1,75 @@
+package cmd
+
+import (
+	"context"
+	"time"
+
+	"github.com/zerops-io/zcli/src/proto/business"
+
+	"github.com/zerops-io/zcli/src/constants"
+
+	"github.com/zerops-io/zcli/src/cliAction/startStopDeleteProject"
+	"github.com/zerops-io/zcli/src/i18n"
+	"github.com/zerops-io/zcli/src/utils/httpClient"
+	"github.com/zerops-io/zcli/src/utils/zipClient"
+
+	"github.com/spf13/cobra"
+)
+
+func serviceStartCmd() *cobra.Command {
+	cmdStart := &cobra.Command{
+		Use:          "start [projectName] [serviceName]",
+		Short:        i18n.CmdServiceStart,
+		Args:         cobra.MinimumNArgs(2),
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx, cancel := context.WithCancel(context.Background())
+			regSignals(cancel)
+
+			storage, err := createCliStorage()
+			if err != nil {
+				return err
+			}
+
+			region, err := createRegionRetriever()
+			if err != nil {
+				return err
+			}
+
+			reg, err := region.RetrieveFromFile()
+			if err != nil {
+				return err
+			}
+
+			apiClientFactory := business.New(business.Config{
+				CaCertificateUrl: reg.CaCertificateUrl,
+			})
+			apiGrpcClient, closeFunc, err := apiClientFactory.CreateClient(
+				ctx,
+				reg.GrpcApiAddress,
+				getToken(storage),
+			)
+			if err != nil {
+				return err
+			}
+			defer closeFunc()
+
+			client := httpClient.New(httpClient.Config{
+				HttpTimeout: time.Minute * 15,
+			})
+
+			zip := zipClient.New(zipClient.Config{})
+
+			return startStopDeleteProject.New(
+				startStopDeleteProject.Config{},
+				client,
+				zip,
+				apiGrpcClient,
+			).Run(ctx, startStopDeleteProject.RunConfig{
+				ProjectName: args[0],
+				ServiceName: args[1],
+			}, constants.Start)
+		},
+	}
+	return cmdStart
+}
