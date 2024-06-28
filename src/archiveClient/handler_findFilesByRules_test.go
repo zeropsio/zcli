06c033ce5d6bd87a859cc70de27d963ca47f64cc
Matěj Pavlíček <tikinang@gmail.com>
2024-06-28 10:35:07 +0200

    test: add tests to .deployignore feature

--- src/archiveClient/handler_findFilesByRules_test.go
+++ src/archiveClient/handler_findFilesByRules_test.go
@@ -4,16 +4,18 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	ignore "github.com/sabhiram/go-gitignore"
 	"github.com/stretchr/testify/require"
 
 	"github.com/zeropsio/zcli/src/uxBlock/mocks"
 )
 
 var findByRulesTestCases = []struct {
-	name       string
-	workingDir string
-	input      []string
-	output     []string
+	name        string
+	workingDir  string
+	input       []string
+	output      []string
+	ignoreLines []string
 }{
 
 	{
@@ -221,6 +223,126 @@ var findByRulesTestCases = []struct {
 			"file3.3.symlink.txt",
 		},
 	},
+
+	// ignore file tests
+	{
+		name:       "ignore - no ignore",
+		workingDir: "test2/var/www/",
+		input: []string{
+			"./",
+		},
+		output: []string{
+			".env",
+			".env.dist",
+			"dir/",
+			"dir/file4.ext1",
+			"dir2/",
+			"dir2/dev.env",
+			"dir3/",
+			"dir3/file3.ext2",
+			"dir3/file4.ext2",
+			"dir3/subDir/",
+			"dir3/subDir/file5.ext1",
+			"dir3/subDir/subSubDir/",
+			"dir3/subDir/subSubDir/file6.ext2",
+			"file1.ext1",
+			"file2.ext2",
+			"file3.ext2",
+		},
+	},
+	{
+		name:       "ignore - everything excluding file1.ext1",
+		workingDir: "test2/var/www/",
+		input: []string{
+			"./",
+		},
+		ignoreLines: []string{
+			"*",
+			"!file1.ext1",
+		},
+		output: []string{
+			"file1.ext1",
+		},
+	},
+	{
+		name:       "ignore - ext2 but not in dir3",
+		workingDir: "test2/var/www/",
+		input: []string{
+			"./",
+		},
+		ignoreLines: []string{
+			"*.ext2",
+			"!dir3/*.ext2",
+		},
+		output: []string{
+			".env",
+			".env.dist",
+			"dir/",
+			"dir/file4.ext1",
+			"dir2/",
+			"dir2/dev.env",
+			"dir3/",
+			"dir3/file3.ext2",
+			"dir3/file4.ext2",
+			"dir3/subDir/",
+			"dir3/subDir/file5.ext1",
+			"dir3/subDir/subSubDir/",
+			"file1.ext1",
+		},
+	},
+	{
+		name:       "ignore - dir wildcard",
+		workingDir: "test2/var/www/",
+		input: []string{
+			"./",
+		},
+		ignoreLines: []string{
+			"dir3/*/subSubDir",
+		},
+		output: []string{
+			".env",
+			".env.dist",
+			"dir/",
+			"dir/file4.ext1",
+			"dir2/",
+			"dir2/dev.env",
+			"dir3/",
+			"dir3/file3.ext2",
+			"dir3/file4.ext2",
+			"dir3/subDir/",
+			"dir3/subDir/file5.ext1",
+			"file1.ext1",
+			"file2.ext2",
+			"file3.ext2",
+		},
+	},
+	{
+		name:       "ignore - .env",
+		workingDir: "test2/var/www/",
+		input: []string{
+			"./",
+		},
+		ignoreLines: []string{
+			"*.env",
+			"!.env.dist",
+		},
+		output: []string{
+			".env.dist",
+			"dir/",
+			"dir/file4.ext1",
+			"dir2/",
+			"dir3/",
+			"dir3/file3.ext2",
+			"dir3/file4.ext2",
+			"dir3/subDir/",
+			"dir3/subDir/file5.ext1",
+			"dir3/subDir/subSubDir/",
+			"dir3/subDir/subSubDir/file6.ext2",
+			"file1.ext1",
+			"file2.ext2",
+			"file3.ext2",
+		},
+	},
 }
 
 func TestFindFilesByRules(t *testing.T) {
@@ -239,7 +361,12 @@ func TestFindFilesByRules(t *testing.T) {
 		t.Run(test.name+"-in-"+test.workingDir, func(t *testing.T) {
 			archiver := New(Config{})
 
-			files, err := archiver.FindFilesByRules(uxBlocks, test.workingDir, test.input, nil)
+			var ignorer FileIgnorer
+			if test.ignoreLines != nil {
+				ignorer = ignore.CompileIgnoreLines(test.ignoreLines...)
+			}
+
+			files, err := archiver.FindFilesByRules(uxBlocks, test.workingDir, test.input, ignorer)
 			require.NoError(t, err)
 
 			output := make([]string, 0, len(files))
