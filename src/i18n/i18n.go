45906333d9ac06ea43fce6c71d8b7db8c91838de
dinesh <talwadkerdinesh@gmail.com>
2024-10-10 00:02:19 +0530

    fix(vpn): added support link, mail & discord link

--- src/i18n/i18n.go
+++ src/i18n/i18n.go
@@ -13,7 +13,8 @@ func T(textConst string, args ...interface{}) string {
 	return translation
 }
 
-const CustomerSupportLink = "support@zerops.io"
+const CustomerSupportLink = "https://support.zerops.io"
+const CustomerSupportEmail = "support@zerops.io"
 const DiscordCommunityLink = "https://discord.com/invite/WDvCZ54"
 
 const (
